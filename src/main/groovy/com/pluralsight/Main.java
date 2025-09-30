41cac8c5f833b2524ff0b92da09ba4670300f853
jesicamatul <evelinmtlj@gmail.com>
2025-09-30 15:41:41 -0400

    final result

--- src/main/groovy/com/pluralsight/Main.java
+++ src/main/groovy/com/pluralsight/Main.java
@@ -3,28 +3,37 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){ // asking variables
+    public static void main(String[] args) { // asking variables
         Scanner scanner = new Scanner(System.in);
-String firstName = enterData("Enter your first name: ").trim();
-String middleName = enterData("Enter your Middle name initial (If no middle name just enter): ").trim();
-String lastName = enterData("Enter your last name: ").trim();
-String suffix = enterData("Enter your suffix (If no suffix just enter: ").trim();
+        String firstName = enterData("Enter your first name: ").trim();
+        String middleName = enterData("Enter your Middle name initial (If no middle name just enter): ").trim();
+        String lastName = enterData("Enter your last name: ").trim();
+        String suffix = enterData("Enter your suffix (If no suffix just enter: ").trim();
 
-String fullName = firstName + " " + lastName;
-if (!middleName.isEmpty()) {
-    System.out.print("Your full name is " + fullName);
-    if (!suffix.isEmpty()){
-        System.out.print(fullName);
-    }
-}
-    }
+        String fullName = firstName;
+        if (middleName.length() > 0) {
+            fullName = firstName + " " + middleName +".";
+        }
+        fullName = fullName + " " + lastName;
+        if (!suffix.isEmpty()) {
+            fullName = fullName + " " + "," + suffix ;
+        }  {
+            System.out.print("Your full name is " + fullName );
+
+
+        }
 
 
 
-    private static String enterData(String data){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(data + " ");
-        return scanner.nextLine();
 
-    } // creating private to ask input
-}
+        }  private static String enterData (String data){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(data + " ");
+            return scanner.nextLine();
+    }         }
+
+
+
+
+        // creating private to ask input
+
